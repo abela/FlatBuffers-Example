088ef85ece60443da5ef93f28316f04f7cb653eb
Giorgi Abelashvili <giorgi.abelashvili@gmail.com>
2018-12-10 12:46:10 +0400

    Initial Commit - Empty Project created

---
+++ FlatBuffers-Example/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FlatBuffers-Example
+//
+//  Created by Giorgi Abelashvili on 12/10/18.
+//  Copyright © 2018 Giorgi Abelashvili. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
